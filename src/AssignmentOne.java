763a237bb10ab6f9a673c0090b36265b8d1718f9
Waxu123 <162484478+Waxu123@users.noreply.github.com>
2024-11-15 13:10:43 +0800

    完成part5

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 // "Part1:HealthProfessional";
 class HealthProfessional {
@@ -89,9 +90,10 @@ class Appointment {
 
 // "AssignmentOne";
 public class AssignmentOne {
- 
+    // "Part5:appointmentList存放Appointment";
+    static ArrayList<Appointment> appointmentList = new ArrayList<>();
 
-    // "main";
+    // "运行主函数main";
     public static void main(String[] args) {
         // "Part3:创建三名GeneralPractitioner和两名Gastroenterology";
         GeneralPractitioner General1 = new GeneralPractitioner(1, "Taylor", "Pediatric care");
@@ -112,7 +114,65 @@ public class AssignmentOne {
         Gastroenterology2.printDetails();
         System.out.println("------------------------------");
 
+        // "Part5:添加2个GeneralPractitioner 2个Gastroenterology的预约到appointmentList";
+        System.out.println("第五部分");
+        createAppointment("John Doe", "1234567890", "10:00", General1);
+        createAppointment("Jane Smith", "0987654321", "11:00", General2);
+        createAppointment("Mark Brown", "1122334455", "14:30", Gastroenterology1);
+        createAppointment("Mark", "1234999999", "14:30", Gastroenterology2);
 
+        // "Part5:输出已预约的列表";
+        printExistingAppointments();
+        // "Part5:取消预约";
+        cancelBooking("1234567890");
+        // "Part5:取消后更新的预约列表";
+        printExistingAppointments();
+        // "Part5:取消不存在的";
+        cancelBooking("1212121212");
     }
 
+    // "Part5:创建预约createAppointment";
+    public static void createAppointment(String name, String phone, String time, HealthProfessional doctor) {
+        // "生成appointment";
+        Appointment appointment = new Appointment(name, phone, time, doctor);
+        // "添加到appointmentList";
+        appointmentList.add(appointment);
+        // "添加成功";
+        System.out.println("create appointment success!");
     }
+
+    // "Part5:输出已预约列表printExistingAppointments";
+    public static void printExistingAppointments() {
+        // "判断预约列表是否为空";
+        if (appointmentList.isEmpty()) {
+            System.out.println("please add appointment");
+            return;
+        }
+
+        // "不是空的就打印预约列表appointmentList";
+        for (int i = 0; i < appointmentList.size(); i++) {
+            appointmentList.get(i).printDetails();
+            System.out.println("------------------------------");
+        }
+    }
+
+    // "Part5:取消预约cancelBooking";
+    public static void cancelBooking(String phone) {
+        int index = -1;
+        for (int i = 0; i < appointmentList.size(); i++) {
+            // "在预约列表里找是否有该手机号的预约equals比较phone是否相等;"
+            if (appointmentList.get(i).phone.equals(phone)) {
+                // "找出对应的位置index";
+                index = i;
+                break;
+            }
+        }
+        // "index如果大于-1找到就remove";
+        if (index > -1) {
+            appointmentList.remove(index);
+            System.out.println("cancel: " + phone);
+        } else {
+            System.out.println("error:" + phone);
+        }
+    }
+}
